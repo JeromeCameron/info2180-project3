8626cd5f8109d514715c91e8c80f772df56534c2
Jerome Cameron <jerome.cameron@mymona.uwi.edu>
2018-11-24 19:49:22 +0000

    added PHP and JS files for dashboard

---
+++ dashboard.js
@@ -0,0 +1,25 @@
+// JavaScript File
+
+window.onload = function(){
+            
+        let httpRequest;
+        
+        makeRequest()
+        
+        function makeRequest(){
+            httpRequest = new XMLHttpRequest();
+            httpRequest.onreadystatechange = request;
+            httpRequest.open('POST', "/dashboard.php", true);
+            httpRequest.send();
+        }
+        
+        function request(){
+            if (httpRequest.readyState === XMLHttpRequest.DONE){
+                if (httpRequest.status === 200){
+                    document.getElementById('openJobs').innerHTML = httpRequest.responseText;
+                } else {
+                    alert('There was a problem with the request.');
+                }
+            }
+        }
+};
--- dashboard.php
+++ dashboard.php
@@ -8,10 +8,14 @@ $dbname = 'hireme';
 
 $conn = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
 
+$stmt = $conn->query("SELECT * FROM jobs");
 
+$results = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
-
-
-
+echo '<ul>';
+foreach ($results as $row) {
+  echo '<li>' . $row['company_name'] . $row['job_title'] . $row['category'] . $row['date_posted'] . '</li>';
+}
+echo '</ul>';
 
 ?>
