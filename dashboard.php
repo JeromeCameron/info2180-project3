d0587b6216fdefd3b66f6471303ce84cd4f2b987
Jerome Cameron <jerome.cameron@mymona.uwi.edu>
2018-11-24 21:03:03 +0000

    updated dashboard.php

--- dashboard.php
+++ dashboard.php
@@ -1,6 +1,5 @@
 <?php
 
-
 $host = getenv('IP');
 $username = getenv('C9_USER');
 $password = '';
@@ -12,10 +11,50 @@ $stmt = $conn->query("SELECT * FROM jobs");
 
 $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
-echo '<ul>';
+echo '<h2>Available Jobs</h2>';
+echo '<table>';
+
+echo '<tr>';
+echo '<th>'. "Company" . '</th>';
+echo '<th>'. "Job Title" . '</th>';
+echo '<th>'. "Category" . '</th>';
+echo '<th>'. "Date" . '</th>';
+echo '</tr>';
 foreach ($results as $row) {
-  echo '<li>' . $row['company_name'] . $row['job_title'] . $row['category'] . $row['date_posted'] . '</li>';
+  echo '<tr>';
+  echo '<td>' . $row['company_name'] . '</td>';
+  echo '<td>' . $row['job_title'] . '</td>';
+  echo '<td>' . $row['category'] . '</td>';
+  echo '<td>' . $row['date_posted'] . '</td>';
+  echo '</tr>';
 }
-echo '</ul>';
+
+echo'</table>';
+
+//Code for jobs a particular user applied for. Need to set up sessions to pull user ID for where clause
+
+$stmt = $conn->query("SELECT company_name, job_title, category, date_applied FROM jobs JOIN jobsAppliedFor ON jobs.id = jobsAppliedFor.job_id where jobsAppliedFor.user_id = 3");
+
+$results = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+echo '<h2>Jobs Applied For</h2>';
+echo '<table>';
+
+echo '<tr>';
+echo '<th>'. "Company" . '</th>';
+echo '<th>'. "Job Title" . '</th>';
+echo '<th>'. "Category" . '</th>';
+echo '<th>'. "Date" . '</th>';
+echo '</tr>';
+foreach ($results as $row) {
+  echo '<tr>';
+  echo '<td>' . $row['company_name'] . '</td>';
+  echo '<td>' . $row['job_title'] . '</td>';
+  echo '<td>' . $row['category'] . '</td>';
+  echo '<td>' . $row['date_applied'] . '</td>';
+  echo '</tr>';
+}
+
+echo'</table>';
 
 ?>
