40c9c5fc8ab4a724d21324942cdd11fd33a54a93
Jerome Cameron <jerome.cameron@mymona.uwi.edu>
2018-11-25 19:51:28 +0000

    update login page

--- login.php
+++ login.php
@@ -1,3 +1,10 @@
 <?php
 
+$host = getenv('IP');
+$username = getenv('C9_USER');
+$password = '';
+$dbname = 'hireme';
+
+$conn = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
+
 ?>
