7cf08697bfd5b96819e00ee11c6ca475646f5809
Jerome Cameron <jerome.cameron@mymona.uwi.edu>
2018-11-25 16:45:47 +0000

    Add PHP validation to user and job froms

---
+++ addJob.php
@@ -0,0 +1,38 @@
+<?php
+
+$host = getenv('IP');
+$username = getenv('C9_USER');
+$password = '';
+$dbname = 'hireme';
+
+$conn = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
+
+if ($_SERVER["REQUEST_METHOD"] == "POST"){
+    
+    $stmt = $conn->prepare("INSERT INTO jobs(job_title,job_description,category,company_name,company_location,date_posted) 
+    VALUES(:title,:jobInfo,:category,:company,:location,:ddate)");
+    
+    $stmt->bindParam(':title', $title);
+    $stmt->bindParam(':jobInfo', $jobInfo);
+    $stmt->bindParam(':category', $category);
+    $stmt->bindParam(':company', $company);
+    $stmt->bindParam(':location', $location);
+    $stmt->bindParam(':ddate', $ddate);
+    
+    $title = clean_input($_POST['title']);
+    $jobInfo = clean_input($_POST['jobInfo']);
+    $category = clean_input($_POST['category']);
+    $company = clean_input($_POST['company']);
+    $location = clean_input($_POST['location']);
+    $ddate = date("Y-m-d");
+    $stmt->execute();
+}
+
+function clean_input($data) {
+  $data = trim($data);
+  $data = stripslashes($data);
+  $data = htmlentities($data);
+  return $data;
+}
+
+?>
--- addUser.php
+++ addUser.php
@@ -7,22 +7,36 @@ $dbname = 'hireme';
 
 $conn = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
 
-$stmt = $conn->prepare("INSERT INTO users(firstname,lastname,password,telephone,email,date_joined) 
-VALUES(:firstname,:lastname,:password,:telephone,:email,:ddate)");
-$stmt->bindParam(':firstname', $firstname);
-$stmt->bindParam(':lastname', $lastname);
-$stmt->bindParam(':password', $password);
-$stmt->bindParam(':telephone', $telephone);
-$stmt->bindParam(':email', $email);
-$stmt->bindParam(':ddate', $ddate);
+
+if ($_SERVER["REQUEST_METHOD"] == "POST"){
+    
+    $stmt = $conn->prepare("INSERT INTO users(firstname,lastname,password,telephone,email,date_joined) 
+    VALUES(:firstname,:lastname,:password,:telephone,:email,:ddate)");
+    $stmt->bindParam(':firstname', $firstname);
+    $stmt->bindParam(':lastname', $lastname);
+    $stmt->bindParam(':password', $password);
+    $stmt->bindParam(':telephone', $telephone);
+    $stmt->bindParam(':email', $email);
+    $stmt->bindParam(':ddate', $ddate);
+    
+    $firstname = clean_input($_POST['firstname']);
+    $lastname = clean_input($_POST['lastname']);
+    $password = clean_input($_POST['password']);
+    $telephone = clean_input($_POST['telephone']);
+    $email = clean_input($_POST['email']);
+    $ddate = date("Y-m-d");
+    $stmt->execute();
+}
+
+function clean_input($data) {
+  $data = trim($data);
+  $data = stripslashes($data);
+  $data = htmlentities($data);
+  return $data;
+}
 
 
-$firstname = $_POST['firstname'];
-$lastname = $_POST['lastname'];
-$password = $_POST['password'];
-$telephone = $_POST['telephone'];
-$email = $_POST['email'];
-$ddate = date("Y-m-d");
-$stmt->execute();
+
+
 
 ?>
