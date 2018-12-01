2a22a84ddbf3f45b35e12ac6f0bb4a4598e505ac
Jerome Cameron <jerome.cameron@mymona.uwi.edu>
2018-12-01 18:35:36 +0000

    checking user form for blank input fields

--- addUser.js
+++ addUser.js
@@ -1,57 +1,72 @@
 // JavaScript File
 
 // JavaScript File
-
-window.onload = function(){
+$(document).ready(function(){
             
         let httpRequest;
+        let numExp = /(?=.*[a-z])(?=.*[A-Z])(?=.*\d).{8,}/;
         
         let button = document.getElementById("button").addEventListener("click", function(event){
             event.preventDefault();
             
-            
-            let firstname = document.getElementById("firstname").value;
-            let lastname = document.getElementById("lastname").value; 
-            let password = document.getElementById("password").value; 
-            let email = document.getElementById("email").value; 
-            let telephone = document.getElementById("telephone").value;
+            //Variable inputs from form
+            let firstname = document.getElementById("firstname");
+            let lastname = document.getElementById("lastname"); 
+            let password = document.getElementById("password"); 
+            let email = document.getElementById("email"); 
+            let telephone = document.getElementById("telephone");
             let token = document.getElementById("token").getAttribute("value");
+            let name;
+            let data = {firstname: firstname, lastname: lastname, password: password, email: email, telephone: telephone};
             
             //Code to test all data for validity goes here
             
             
-            let data = {firstname: firstname, lastname: lastname, password: password, email: email, telephone: telephone, token: token};
-          
-            prepData();
-        
-            function prepData(){
-                httpRequest = new XMLHttpRequest();
-                let encodedData = "";
-                let encodedDataPairs = [];
-                let name;
-                
                 for(name in data) {
-                    encodedDataPairs.push(encodeURIComponent(name) + '=' + encodeURIComponent(data[name]));
-                }
-                
-                encodedData = encodedDataPairs.join('&').replace(/%20/g, '+');
-                
-                httpRequest.onreadystatechange = sendData;
-                httpRequest.open('POST', "/addUserProcess.php");
-                httpRequest.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
-                httpRequest.send(encodedData);
-            }
-            
-            function sendData(){
-                if (httpRequest.readyState === XMLHttpRequest.DONE){
-                    if (httpRequest.status === 200){
-                         document.getElementById('message').innerHTML = httpRequest.responseText;
-                    } else {
-                        alert('There was a problem with the request.');
+                        blankField(data[name]);
+                     }
+                 
+                function blankField(ele){
+                    if (ele.value === ""){
+                        ele.classList.add("invalid");
+                        ele.insertAdjacentHTML('afterend', "<p class = 'message'>Field can't be blank</p>");
+                    }else{
+                        $("p").remove(".message");
+                        //ele.classList.remove("invalid");
                     }
                 }
-            }
+                    
+            // data = {firstname: firstname.value, lastname: lastname.value, password: password.value, email: email.value, telephone: telephone.value, token: token};
+            // prepData();
+        
+            // function prepData(){
+            //     httpRequest = new XMLHttpRequest();
+            //     let encodedData = "";
+            //     let encodedDataPairs = [];
+
+                
+            //     for(name in data) {
+            //         encodedDataPairs.push(encodeURIComponent(name) + '=' + encodeURIComponent(data[name]));
+            //     }
+                
+            //     encodedData = encodedDataPairs.join('&').replace(/%20/g, '+');
+                
+            //     httpRequest.onreadystatechange = sendData;
+            //     httpRequest.open('POST', "/addUserProcess.php");
+            //     httpRequest.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+            //     httpRequest.send(encodedData);
+            // }
+            
+            // function sendData(){
+            //     if (httpRequest.readyState === XMLHttpRequest.DONE){
+            //         if (httpRequest.status === 200){
+            //              document.getElementById('message').innerHTML = httpRequest.responseText;
+            //         } else {
+            //             alert('There was a problem with the request.');
+            //         }
+            //     }
+            // }
             
         })
         
-};
+});
--- login.php
+++ login.php
@@ -1,5 +1,23 @@
 <?php
+ob_start();
+   session_start();
 
+$msg = '';
+            
+            if (isset($_POST['login']) && !empty($_POST['username']) 
+               && !empty($_POST['password'])) {
+				
+               if ($_POST['username'] == 'tutorialspoint' && 
+                  $_POST['password'] == '1234') {
+                  $_SESSION['valid'] = true;
+                  $_SESSION['timeout'] = time();
+                  $_SESSION['username'] = 'tutorialspoint';
+                  
+                  echo 'You have entered valid use name and password';
+               }else {
+                  $msg = 'Wrong username or password';
+               }
+            }
 $host = getenv('IP');
 $username = getenv('C9_USER');
 $password = '';
